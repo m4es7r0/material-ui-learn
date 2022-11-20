b72b2d4698527a15e879757eeb6106ac363ddc99
Musiichenko Dmitry <dmitrymusiichenko@gmail.com>
2022-11-20 16:32:09 +0200

    Radio Buttons

--- src/components/MuiRadioButton.tsx
+++ src/components/MuiRadioButton.tsx
@@ -5,21 +5,31 @@ import {
   FormControl,
   FormControlLabel,
   FormLabel,
+  FormHelperText,
   RadioGroup,
   Radio,
 } from "@mui/material";
 
 export default function MuiRadioButton() {
+  const [value, setValue] = React.useState("");
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setValue(event.target.value);
+  };
+  console.log(value);
+
   return (
     <Stack className="bg-[#ffffffde] container w-full p-6 rounded place-items-center">
       <Box>
-        <FormControl>
+        <FormControl error={value === "0-2" ? true : false}>
           <FormLabel id="job-experience-group-label">
             Years of experience
           </FormLabel>
           <RadioGroup
             name="job-experience-group"
             aria-labelledby="job-experience-group-label"
+            row
+            value={value}
+            onChange={handleChange}
           >
             <FormControlLabel
               className="text-[#242424]"
@@ -40,6 +50,11 @@ export default function MuiRadioButton() {
               value="6-8"
             />
           </RadioGroup>
+          {value === "0-2" ? (
+            <FormHelperText variant="standard">
+              Invalid Selection
+            </FormHelperText>
+          ) : null}
         </FormControl>
       </Box>
     </Stack>
