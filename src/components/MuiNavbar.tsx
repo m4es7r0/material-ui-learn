d29e563f16319d506e8857a1d58f9afd178d5da3
Musiichenko Dmitry <dmitrymusiichenko@gmail.com>
2022-11-24 21:52:46 +0200

    ...

--- src/components/MuiNavbar.tsx
+++ src/components/MuiNavbar.tsx
@@ -11,28 +11,28 @@ import CatchingPokemonIcon from "@mui/icons-material/CatchingPokemon";
 
 export default function MuiNavbar() {
   return (
-    <Stack className="bg-[#ffffffde] text-[#242424] p-6 rounded w-full">
-      <AppBar position="static">
-        <Toolbar>
-          <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="Pokeboll Logo"
-          >
-            <CatchingPokemonIcon />
-          </IconButton>
-          <Typography variant="h5" component="div" sx={{ flexGrow: 1 }}>
-            Material UI
-          </Typography>
-          <Stack direction="row" spacing={2}>
-            <Button color="inherit">Features</Button>
-            <Button color="inherit">Pricing</Button>
-            <Button color="inherit">About</Button>
-            <Button color="inherit">Login</Button>
-          </Stack>
-        </Toolbar>
-      </AppBar>
-    </Stack>
+    // <Stack className="bg-[#ffffffde] text-[#242424] p-6 rounded w-full">
+    <AppBar position="fixed">
+      <Toolbar>
+        <IconButton
+          size="large"
+          edge="start"
+          color="inherit"
+          aria-label="Pokeboll Logo"
+        >
+          <CatchingPokemonIcon />
+        </IconButton>
+        <Typography variant="h5" component="div" sx={{ flexGrow: 1 }}>
+          Material UI
+        </Typography>
+        <Stack direction="row" spacing={2}>
+          <Button color="inherit">Features</Button>
+          <Button color="inherit">Pricing</Button>
+          <Button color="inherit">About</Button>
+          <Button color="inherit">Login</Button>
+        </Stack>
+      </Toolbar>
+    </AppBar>
+    // </Stack>
   );
 }
